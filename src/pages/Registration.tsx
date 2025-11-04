1706acd882b3e1a2e5070e5dedcb796de705b081
Randy Beltran <beltran.bsinfotech@gmail.com>
2025-11-04 09:05:46 +0800

    update: remove .env setup

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -106,10 +106,10 @@ const Registration: React.FC = () => {
       };
 
       await emailjs.send(
-        import.meta.env.VITE_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        'service_eej4xp7',
+        'template_04iax4l',
         emailParams,
-        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+        'gwBiOtZu_hHDvcA_Y'
       );
 
       // 8. Sign out the user
